3cece46809ef531c4e9e627ed4a7e18e67b6325a
Matthew Hyrka <mhyrka@gmail.com>
2018-04-22 16:52:07 -0600

    First commit. Only styled for mobile.

---
+++ app.js
@@ -0,0 +1,30 @@
+const get_price_url = 'https://api.coindesk.com/v1/bpi/currentprice/USDjson'
+
+function append(parent, child) {
+  return parent.appendChild(child)
+}
+
+let body = document.querySelector('body')
+
+let currentPriceSection = document.querySelector('.current-price')
+let price = document.createElement('h2')
+append(currentPriceSection, price)
+
+
+function getPrice() {
+  fetch(get_price_url)
+  .then(response => response.json())
+  .then(response => displayPrice(response))
+}
+
+function displayPrice(data) {
+  let currentPrice = data.bpi.USD.rate_float
+  price.textContent = currentPrice
+  console.log(price.textContent)
+}
+
+window.setInterval(function(){
+  getPrice()
+}, 1000)
+
+getPrice()
